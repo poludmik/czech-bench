b6409eae302332c8acb5b197a0e388a155b60786
Adam Jirkovský <jirkovsky.adam@gmail.com>
2024-05-09 11:48:03 +0000

    Main

--- models/auto_hf.py
+++ models/auto_hf.py
@@ -1,5 +1,5 @@
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from transformers import pipeline
+from transformers import pipeline, QuantoConfig
 import torch
 
 
@@ -14,6 +14,10 @@ def get_llm(model_id="google/flan-t5-xl", task=None, do_sample=False, max_new_to
             kwargs["torch_dtype"] = torch.float32
         elif precision == "auto":
             kwargs["torch_dtype"] = 'auto'
+        elif precision == "fp8":
+            kwargs["quantization_config"] = QuantoConfig(weights="float8")
+        elif precision in ["int8", "int4", "int2"]:
+            kwargs["quantization_config"] = QuantoConfig(weights=precision)
         else:
             print(f"Unknown precision '{precision}', setting to 'auto'")
             kwargs["torch_dtype"] = 'auto'
